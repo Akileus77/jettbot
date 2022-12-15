268d8e912d41671d78b3275e2f38920eb7064dcf
Akileus77 <Akikami77@gmail.com>
2022-12-15 20:45:30 +0300

    Delete commands directory

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-const pingCommand = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('pong') 
-
-export default pingCommand.toJSON();
